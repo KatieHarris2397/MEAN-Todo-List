84a8bd031fcaf674d278a0d5262855f49051cdaa
Katie Harris <katie.harris2397@gmail.com>
2024-11-20 14:32:45 -0500

    Add mongodb url env var for express server

--- express/dbConfig.js
+++ express/dbConfig.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 async function connect() {
     try {
-        const res = await mongoose.connect("mongodb://mongo:passw0rd@127.0.0.1:27017/todo");
+        const res = await mongoose.connect(process.env.MONGODB_URL);
         console.log("DB Connected >>>");
     } catch (err) {
         console.log(err);
